57346212e15e8a4fc67cea7e662b5c6e7daabd0e
Jakob <jakob@MacBook-Pro-von-Jakob.local>
2026-09-18 20:52:52 +0200

    docs: some basic docs about slices in go

--- arrays-slices/main.go
+++ arrays-slices/main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	/* little Summary:
-
-	 */
+	Declaring and inializing syntax:
+	arr_name := [arr_size]type{values}
+	len(arr) -> len of arr
+	cap(arr) -> capacity of arr
+	*/
 	arr := [3]int{1, 2, 3}
 	fmt.Println(arr)
+	fmt.Println((len(arr)))
 }
